cbac5e4e4a6cdf898693b044f060610a5156cc48
Daniel Erhabor <danielnerhabor@gmail.com>
2022-06-21 22:02:52 -0400

    open modal on clicking a task list item

--- src/components/task/TaskItem.tsx
+++ src/components/task/TaskItem.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useAppDispatch } from '../../app/hooks';
+import { setIsOpen } from '../../features/modalSlice';
 
 import { Task } from '../../types/types';
 
 const TaskItem: React.FC<{ task: Task }> = ({ task }) => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(setIsOpen(true));
+  };
+
   return (
-    <li>
+    <li onClick={handleClick}>
       {task.id} {task.description} {task.created_at.toDateString()}{' '}
       {task.status}
     </li>
