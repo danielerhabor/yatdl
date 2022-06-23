d66cdbf2dfa0f0a63ec7fee4472e503570a7e9dc
Daniel Erhabor <danielnerhabor@gmail.com>
2022-06-22 21:36:42 -0400

    On save of modal print out the modal description and the title

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { useAppDispatch } from '../../app/hooks';
 import { setIsOpen } from '../../features/modalSlice';
@@ -6,8 +7,28 @@ import styles from './Modal.module.css';
 
 const Modal: React.FC = () => {
   const dispatch = useAppDispatch();
+  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState('');
+
   const closeModal = () => {
+    // clear the title and description
+    clearModal();
     dispatch(setIsOpen(false));
+  };
+
+  const clearModal = () => {
+    setDescription('');
+    setTitle('');
+  };
+
+  const saveHandler = () => {
+    // print the title of the modal
+    // and the text description
+    console.log(title);
+    console.log(description);
+
+    // close the modal
+    closeModal();
   };
 
   const domModal = document.getElementById('root-modal') as HTMLElement;
@@ -16,6 +37,8 @@ const Modal: React.FC = () => {
       <div className={styles.modalContainer}>
         <header className="modalHeader">
           <input
+            
+            onChange={e => setTitle(e.target.value)}
             className="modalTaskName"
             placeholder="Enter task name..."
           ></input>
@@ -24,12 +47,17 @@ const Modal: React.FC = () => {
           </button>
         </header>
         <textarea
+          onChange={e => setDescription(e.target.value)}
           className="modalTextArea"
           placeholder="Enter task description..."
         ></textarea>
         <footer className="modalFooter">
-          <button className="modalSaveButton">Save</button>
-          <button onClick={closeModal} className="modalCancelbutton">Cancel</button>
+          <button onClick={saveHandler} className="modalSaveButton">
+            Save
+          </button>
+          <button onClick={closeModal} className="modalCancelbutton">
+            Cancel
+          </button>
         </footer>
       </div>
     </div>
